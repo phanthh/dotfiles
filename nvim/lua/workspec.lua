85c18c3bc8cfcefd0751a66c4964e7cf7870dd40
phanthh <phanthehauah1@gmail.com>
2022-01-30 21:00:01 +0200

    Update: 1643569201

--- nvim/lua/workspec.lua
+++ nvim/lua/workspec.lua
@@ -20,6 +20,7 @@ endfunction
 function s:spec_python()
   nnoremap <s-f9> :vsplit term://prime-run ipython<bar>:wincmd h<cr>
   nnoremap <f9> :split term://prime-run ipython<bar>:wincmd h<cr>
+  nnoremap <s-f10> :!jupytext --to notebook %<cr><cr>
   " call s:repl_spec()
 endfunction
 
