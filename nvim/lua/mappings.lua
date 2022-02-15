7e4db67cc0c5cedd1354529bb594dd9ea34d41f4
phanthh <phanthehauah1@gmail.com>
2022-02-15 12:00:01 +0200

    Update: 1644919201

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,11 +1,11 @@
 -- Key map
 local km = require("utils").keymap
 local opts = { noremap = true, silent = true }
-km("i", "jj", "<esc><cmd>w<cr>", opts)
+km("i", "jj", "<esc>", opts)
 km("n", "<c-s>", "<cmd>w<cr>", opts)
 km("i", "<c-s>", "<esc><cmd>w<cr>", opts)
-km("n", "j", "gj", opts)
-km("n", "k", "gk", opts)
+-- km("n", "j", "gj", opts)
+-- km("n", "k", "gk", opts)
 km("n", "<c-w>i", ":tabnext<cr>", opts)
 km("n", "<c-w>u", ":tabprevious<cr>", opts)
 km("n", "<c-w>0", ":tabfirst<cr", opts)
