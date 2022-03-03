abecc7f25dbc7b9ad320639680ac2fcb718048d3
phanthh <phanthehauah1@gmail.com>
2022-03-03 12:00:01 +0200

    Update: 1646301601

--- nvim/lua/pluginList.lua
+++ nvim/lua/pluginList.lua
@@ -210,6 +210,19 @@ require("packer").startup({
 								}
 							end,
 						},
+						typescriptreact = {
+							function()
+								return {
+									exe = "prettier",
+									args = {
+										"--stdin-filepath",
+										vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
+										"--single-quote",
+									},
+									stdin = true,
+								}
+							end,
+						},
 					},
 				})
 			end,
