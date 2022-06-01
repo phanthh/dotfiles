3532fccafa817b3f70b1cf0d2a5aab192534ff4e
phanthh <phanthh25701@gmail.com>
2022-06-02 00:00:01 +0300

    Update: 1654117201

--- nvim/lua/plugins/nvim-lsp-ts-utils.lua
+++ nvim/lua/plugins/nvim-lsp-ts-utils.lua
@@ -28,7 +28,7 @@ local function config(client, bufnr)
 		filter_out_diagnostics_by_code = {},
 
 		-- inlay hints
-		auto_inlay_hints = true,
+		auto_inlay_hints = false,
 		inlay_hints_highlight = "Comment",
 		inlay_hints_priority = 200, -- priority of the hint extmarks
 		inlay_hints_throttle = 150, -- throttle the inlay hint request
