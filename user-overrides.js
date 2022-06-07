d131509049a08b4711795f122620db5e8dd1000e
phanthh <phanthh25701@gmail.com>
2022-06-07 18:00:01 +0300

    Update: 1654614001

--- user-overrides.js
+++ user-overrides.js
@@ -8,3 +8,10 @@ user_pref("ui.key.menuAccessKeyFocuses", false);
 user_pref("full-screen-api.warning.timeout", 0);
 // disable picture in picture button
 user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
+
+/* override recipe: enable session restore ***/
+user_pref("browser.startup.page", 3); // 0102
+// user_pref("browser.privatebrowsing.autostart", false); // 0110 required if you had it set as true
+// user_pref("browser.sessionstore.privacy_level", 0); // 1003 optional to restore cookies/formdata
+user_pref("privacy.clearOnShutdown.history", false); // 2811
+// user_pref("privacy.cpd.history", false); // 2812 optional to match when you use Ctrl-Shift-Del
